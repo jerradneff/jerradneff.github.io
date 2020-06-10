3d46ab08fc803eee8711ac42f71b5d60a5f38499
Jerrad Neff <jerrad.n@gmail.com>
2020-06-09 22:16:00 -0700

    More efficient vplogic

--- bakemomma/script.js
+++ bakemomma/script.js
@@ -1,12 +1,19 @@
+
+window.addEventListener("resize", onWindowResize);
 window.addEventListener("scroll", onViewportChange);
-window.addEventListener("resize", onViewportChange);
 
 document.addEventListener("DOMContentLoaded", () => {
-
+    onWindowResize();
 });
 
+var windowHeight;
+
+function onWindowResize() {
+    windowHeight = window.innerHeight;
+    onViewportChange();
+}
+
 function onViewportChange() {
-    var windowHeight = window.innerHeight;
     var parallaxObjects = document.getElementsByClassName('parallax');
     var bound;
     for(i = 0; i < parallaxObjects.length; i++) {
